ddd935503277f813db89a8d969f1f29a3155d1a4
Charles Koutcheme <charles.koutcheme@student.uliege.be>
2020-08-02 21:39:00 +0200

    Updates

---
+++ precache-manifest.4c66beacc2e7a667d95c7c18e5877836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a25eeb0d19d71fbfdfc26a1e37f6497",
+    "url": "/ciel/index.html"
+  },
+  {
+    "revision": "8283c9e5b05189e533a4",
+    "url": "/ciel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87d8101f4f27dbf66032",
+    "url": "/ciel/static/js/2.0338c868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ciel/static/js/2.0338c868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8283c9e5b05189e533a4",
+    "url": "/ciel/static/js/main.cb388e09.chunk.js"
+  },
+  {
+    "revision": "7df17d6c5739bd1b139f",
+    "url": "/ciel/static/js/runtime-main.c5f5f446.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ciel/static/media/logo.5d5d9eef.svg"
+  }
+]);
